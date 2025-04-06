b9fad9854f787ccdc002ba96f675cf615c955f65
kentangkiting <agustinus_bintang@yahoo.com>
2025-04-07 01:30:36 +0700

    homework37_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Form, Field } from "react-final-form";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const required = (name: string) => (value: string) => {
+    return value ? undefined : `${name} dibutuhkan`;
+  };
 
+  const minLength = (min: number) => (value: string) => {
+    return value && value.length < min
+      ? `Harus lebih dari ${min} karakter `
+      : undefined;
+  };
+  const maxLength = (max: number) => (value: string) => {
+    return value && value.length > max
+      ? `Harus Kurang dari ${max} karakter `
+      : undefined;
+  };
+  const composeValidator =
+    (...validators: any[]) =>
+    (value: any) => {
+      return validators.reduce(
+        (error, validator) => error || validator(value),
+        undefined
+      );
+    };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Form
+      onSubmit={(value) => console.log(value)}
+      render={({ handleSubmit, form, submitting }) => (
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="email"
+            component="input"
+            validate={composeValidator(required("Email"))}
+          >
+            {({ input, meta }) => (
+              <div>
+                <input {...input} name="email" placeholder="email" />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+          <Field
+            name="user"
+            component="input"
+            validate={composeValidator(
+              required("User"),
+              minLength(5),
+              maxLength(12)
+            )}
+          >
+            {({ input, meta }) => (
+              <div>
+                <input {...input} name="user" placeholder="user" />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+          <Field
+            name="password"
+            component="input"
+            validate={composeValidator(required("Password"), minLength(6))}
+          >
+            {({ input, meta }) => (
+              <div>
+                <input
+                  {...input}
+                  name="Password"
+                  type="password"
+                  placeholder="Password"
+                />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+          <button type="submit" disabled={submitting}>
+            Submit
+          </button>
+          <button type="button" onClick={form.reset}>
+            Reset
+          </button>
+        </form>
+      )}
+    ></Form>
+  );
 }
 
-export default App
+export default App;
